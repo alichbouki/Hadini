40ea0fa855a9b311f35847980c277dbd3693e00d
Alinexuss <ali.chbouki2007@gmail.com>
2024-11-03 21:39:41 +0100

    Made it so it can login automatically

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-#/bin/python3
+from selenium import webdriver
+from jproperties import Properties
 
-print("Hello this is the first commit's code")
+configs = Properties()
+
+LOGIN_BTN_ID = "#Button1"
+USERNAME_FIELD_ID = "#Username"
+PASSWORD_FIELD_ID = "#Password"
+
+LOGIN_URL = "https://universitic.ueuromed.org/konosys/PC_MV_login.aspx"
+
+with open('.env', 'rb') as config_file:
+    configs.load(config_file)
+
+USERNAME = str(configs.get("USERNAME").data).replace("\"", "")
+PASSWORD = str(configs.get("PASSWORD").data).replace("\"", "")
+
+
+browser = webdriver.Firefox()
+browser.get(LOGIN_URL)
+
+browser.find_element('css selector', USERNAME_FIELD_ID).send_keys(USERNAME)
+browser.find_element('css selector', PASSWORD_FIELD_ID).send_keys(PASSWORD)
+browser.find_element("css selector", LOGIN_BTN_ID).click()
+
+while input() != 'e':
+    pass
+
+browser.quit()
