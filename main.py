10e850f8f624619e1f1fa84411f8e94f2591865d
Alinexuss <ali.chbouki2007@gmail.com>
2024-11-04 15:12:02 +0100

    able to fetch the html source code form the web page and dump it in a html file

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ USERNAME_FIELD_ID = "#Username"
 PASSWORD_FIELD_ID = "#Password"
 
 LOGIN_URL = "https://universitic.ueuromed.org/konosys/PC_MV_login.aspx"
+CALENDAR_URL = "https://universitic.ueuromed.org/konosys/interfaces/MonPlanning_Utilisateur_Lot.aspx"
 
-with open('.env', 'rb') as config_file:
-    configs.load(config_file)
+with open('.env', 'rb') as configFile:
+    configs.load(configFile)
+    configFile.close()
 
 USERNAME = str(configs.get("USERNAME").data).replace("\"", "")
 PASSWORD = str(configs.get("PASSWORD").data).replace("\"", "")
-
 
 browser = webdriver.Firefox()
 browser.get(LOGIN_URL)
@@ -22,6 +23,16 @@ browser.get(LOGIN_URL)
 browser.find_element('css selector', USERNAME_FIELD_ID).send_keys(USERNAME)
 browser.find_element('css selector', PASSWORD_FIELD_ID).send_keys(PASSWORD)
 browser.find_element("css selector", LOGIN_BTN_ID).click()
+
+browser.get(CALENDAR_URL)
+
+htmlSrc = browser.page_source
+
+print(htmlSrc)
+
+with open("./index.html", 'x') as htmlFile:
+    htmlFile.write(htmlSrc)
+    htmlFile.close()
 
 while input() != 'e':
     pass
---
+++ parssing.py
@@ -0,0 +1,5 @@
+def getValues(html: str):
+    
+    
+    
+    pass
